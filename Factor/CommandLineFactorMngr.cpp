004c40332178e18d9607ba7ada50c01ea2105085
zengda <happy_zengda@126.com>
2015-05-14 17:16:01 +0800

    增加搜索内容

---
+++ Factor/CommandLineFactorMngr.cpp
@@ -0,0 +1,28 @@
+#include "CommandLineFactorMngr.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+CommandLineFactorMngr::CommandLineFactorMngr()
+{
+	
+}
+
+void CommandLineFactorMngr::searchAddonFactorFile()
+{
+	char curPath[1024] = {0};
+	char resolvedPaht[1024] = {0};
+	getcwd(curPath,sizeof(curPaht));
+	realPath(curPaht, resolvedPaht);
+	while()
+}
+
+
+void CommandLineFactorMngr::setAddonFactorFileDirectory(string path)
+{
+}
+
+uint32 CommandLineFactorMngr::addonFactorAnalyser()
+{
+}
+
+
--- header/CommandLineFactorMngr.h
+++ header/CommandLineFactorMngr.h
@@ -4,19 +4,23 @@
 #include <vector>
 #include <string>
 #include "JZCommonDefine.h"
-
 using namespace std;
+
+#define DEFAULT_COMMON_LINE_FACTOR_FILE_NAME ".factor"
 
 typedef vector<string> FactorList;
 
 class CommandLineFactorMngr
 {
 public:
+	CommandLineFactorMngr();
 	//this func may need to rewrite for windows
 	void searchAddonFactorFile();
 	
 	void setAddonFactorFileDirectory(string path);
+
 	uint32 addonFactorAnalyser();
+
 private:
 	string mAddonFactorDefineFile
 	FactorList mOriginalFactorList;
