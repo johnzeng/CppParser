7b95be04d425108e3bf54b836f670a09c7859635
zengda <happy_zengda@126.com>
2015-05-17 13:19:33 +0800

    增加符号保存链表节点结构

--- header/LexicalAnalyzer.h
+++ header/LexicalAnalyzer.h
@@ -1,17 +1,43 @@
 #ifndef LEXICALANALYZER_H
-
 #define LEXICALANALYZER_H
 
 #include "JZCommonDefine.h"
 #include <stack>
 using namespace std;
+
+enum SymbolBlockTypeDef
+{
+	eSymBlockGolbal = 1,		//golbal symbol
+	eSymBlockNone = 2,			//only surrounded with {}
+	eSymBlockFunc = 3,			//func block
+	eSymBlockFor = 4,			//for block
+	eSymBlockIf	= 5,			//if block
+	eSymBlockWhile = 6,			//while block
+	eSymBlockDo = 7,			//do block
+}
+
+struct SymbolNode
+{
+	SymbolNode* father;
+	vector<SymbolNode*> child;
+	int blockType;
+	string symbolName;
+	string symbolType;
+	SymbolNode* defineNode;
+};
+
 class LexicalAnalyzer {
 public:
 	LexicalAnalyzer ();
 	virtual ~LexicalAnalyzer ();
 
+	static LexicalAnalyzer* getInstance();
+
+	void init();
+	void setSourceCodeDir(string path);
+	
 private:
-	StringStack mOriginalWords;
+	StringList mOriginalWords;
 	
 };
 
