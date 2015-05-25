e711e8ee2e0186671557f01e43db61426c0e2ecf
zengda <happy_zengda@126.com>
2015-05-25 13:08:06 +0800

    先删掉没想清楚的代码

--- Core/CommentHandler.cpp
+++ Core/CommentHandler.cpp
@@ -27,39 +27,5 @@ CommentHandler* CommentHandler::getInstance()
 
 std::string CommentHandler::eraseComment(const std::string& src)
 {
-	int flag = COMMENT_FLAG_NON;
-	stringstream ioStream;
-	ioStream << src;
-	string line;
-	while(false != getline(ioStream, line))
-	{
-		JZWRITE_DEBUG("get line : %s", line.c_str());
-		for (int i = 0; i < line.size; i++) {
-			if ('\\' == line[i])
-			{
-				//handle "\" here
-
-			}
-			if('"' == line[i] && (flag == COMMENT_FLAG_NON || flag == COMMENT_FLAG_SRC))
-			{
-				//in src code ,now enter the str mode
-				flag = COMMENT_FLAG_STR;
-				continue;	
-			}
-
-			switch(flag)
-			{
-				case COMMENT_FLAG_NON:
-				{
-					break;	
-				}
-				default:
-				{
-					break;	
-				}
-			}			
-		}
-		
-	}
 	return "";
 }
