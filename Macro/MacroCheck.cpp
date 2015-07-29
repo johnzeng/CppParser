21124acba603ed734f3bd3ba93c408f2e362f681
zengda <happy_zengda@126.com>
2015-07-29 12:22:30 +0800

    提交defined的处理，另外处理了一个sharp的问题

---
+++ Macro/MacroCheck.cpp
@@ -0,0 +1,14 @@
+#include "LexData.h"
+#include "Lex.h"
+#include "GrammarUtil.h"
+
+using namespace LexUtil;
+
+uint32 Lex::isMacroSuccess(const LexRecList& logic, bool* ret)
+{
+	*ret = false;
+		
+	return eLexNoError;	
+}
+
+
