4494392993fe0b6791c1744a2a59b348946b280e
Johnzeng <Johnz@misfit.com>
2016-03-30 23:31:13 +0800

    get a bug

--- test/src/LexUtilTest.cpp
+++ test/src/LexUtilTest.cpp
@@ -1,13 +1,16 @@
 #include <gtest/gtest.h>
 #include "Lex.h"
 #include "JZFileUtil.h"
+#include <string.h>
 
 TEST(LexUtil, eraseLineSeperator){
-  StringList fileList = JZGetAllFiles("./test/Samples/LexUtilEraseLineSeperator", false);
-  for(auto i = fileList.begin(); i < fileList.end(); i++)
-  {
-    uint64 buffSize = 0;
-    unsigned char* buff = JZGetFileData(i->c_str(),&buffSize);
-    const char* eraseRet = LexUtil::eraseLineSeperator((const char*)buff, &buffSize);
-  }
+  const char* input1 = "123456\\ \n 12345";
+  uint64 bufSize1 = strlen(input1);
+  const char* eraseRet1 = LexUtil::eraseLineSeperator((const char*)input1, &bufSize1);
+  ASSERT_STREQ("123456 12345", eraseRet1);
+
+  const char* input2 = "\"123456\\ \n 12345\"";
+  uint64 bufSize2 = strlen(input1);
+  const char* eraseRet2 = LexUtil::eraseLineSeperator((const char*)input2, &bufSize2);
+  ASSERT_STREQ("\"123456 12345\"", eraseRet2);
 }
