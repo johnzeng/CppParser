9f4a332789cc27a86a852833470b2004c61c5c1f
Johnzeng <Johnz@misfit.com>
2016-04-02 11:25:46 +0800

    better test mode

--- test/src/GrammarUtilTest.cpp
+++ test/src/GrammarUtilTest.cpp
@@ -7,27 +7,34 @@
 //}
 
 TEST(GrammarUtil, operatorSet){
-  string input1 = "+";
-  EXPECT_EQ(true, GrmUtilPtr->isOperator(input1));
+  StringList input;
+  input.push_back("+");
+  input.push_back("-");
+  input.push_back("*");
+  input.push_back("/");
+  input.push_back("==");
+  input.push_back("!=");
+  input.push_back("<");
+  input.push_back(">");
+  input.push_back("<=");
+  input.push_back(">=");
+  input.push_back("==");
+  input.push_back("!=");
+  input.push_back("<<");
+  input.push_back(">>");
+  input.push_back("%");
+  input.push_back("|");
+  input.push_back("&");
+  for (int i = 0; i < input.size(); i++) 
+  {
+    EXPECT_EQ(true, GrmUtilPtr->isOperator(input[i]));
+  }
 
-  string input2 = "-";
-  EXPECT_EQ(true, GrmUtilPtr->isOperator(input2));
-
-  string input3 = "*";
-  EXPECT_EQ(true, GrmUtilPtr->isOperator(input3));
-
-  string input4 = "/";
-  EXPECT_EQ(true, GrmUtilPtr->isOperator(input4));
-
-  string input5 = "==";
-  EXPECT_EQ(true, GrmUtilPtr->isOperator(input5));
-
-  string input6 = "!=";
-  EXPECT_EQ(true, GrmUtilPtr->isOperator(input6));
-
-  string input7 = "+";
-  EXPECT_EQ(true, GrmUtilPtr->isOperator(input7));
-
-  string input8 = "+";
-  EXPECT_EQ(true, GrmUtilPtr->isOperator(input8));
+  StringList falseInput;
+  falseInput.push_back("abc");
+  falseInput.push_back("+-");
+  for (int i = 0; i < falseInput.size(); i++) 
+  {
+    EXPECT_EQ(false, GrmUtilPtr->isOperator(falseInput[i]));
+  }
 }
