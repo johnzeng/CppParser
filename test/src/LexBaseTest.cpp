37312a4958138c3cf04f606999bc429b57de9faa
Johnzeng <Johnz@misfit.com>
2016-04-15 09:03:31 +0800

    add str test for lex base

--- test/src/LexBaseTest.cpp
+++ test/src/LexBaseTest.cpp
@@ -35,9 +35,46 @@ TEST(LexBase, simpleInput){
 //	JZSetLoggerLevel(JZ_LOG_DEBUG);
 //  lex.printLexRec();
 //	JZSetLoggerLevel(JZ_LOG_TEST);
-  EXPECT_EQ(3, recList.size());
+  ASSERT_EQ(3, recList.size());
   ASSERT_STREQ("int", recList[0].word.c_str());
   ASSERT_STREQ("a", recList[1].word.c_str());
   ASSERT_STREQ(";", recList[2].word.c_str());
+
+}
+
+//cover test: input with string or char
+TEST(LexBase, baseStrTest){
+  //this is really a strong test, so don't do it for more than one file every time
+  int argc = 2;
+  char argv0[128] = {0},argv1[128] = {0};
+  strcpy(argv0,"tester");
+  strcpy(argv1,"./test/TestSet/base_str_test");
+  char* argv[2] = {argv0,argv1};
+
+	//init
+	globalInitAtBegin();
+
+	JZSetLoggerLevel(JZ_LOG_TEST);
+
+	//analyze command line input
+	CmdInputFactor::getInstance()->analyze(argc, argv);
+
+	//now begin to analyze the files input from command line
+	string toCompileFile = CmdInputFactor::getInstance()->getNextFile();
+
+  LexBase lex;
+  lex.analyzeAFile(toCompileFile);
+  LexRecList recList = lex.getRecList();
+
+//	JZSetLoggerLevel(JZ_LOG_DEBUG);
+//  lex.printLexRec();
+//	JZSetLoggerLevel(JZ_LOG_TEST);
+  ASSERT_EQ(6, recList.size());
+  ASSERT_STREQ("main", recList[0].word.c_str());
+  ASSERT_STREQ("(", recList[1].word.c_str());
+  ASSERT_STREQ("\"hello\"", recList[2].word.c_str());
+  ASSERT_STREQ(",", recList[3].word.c_str());
+  ASSERT_STREQ("'w'", recList[4].word.c_str());
+  ASSERT_STREQ(")", recList[5].word.c_str());
 
 }
