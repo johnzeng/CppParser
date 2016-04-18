bd42374f9211a541a713dd26750262451cefa113
Johnzeng <Johnz@misfit.com>
2016-04-18 17:10:10 +0800

    add more test case for var args

--- test/src/MacroLexTest.cpp
+++ test/src/MacroLexTest.cpp
@@ -97,11 +97,8 @@ TEST(MacroLex, macroTest3){
 	string toCompileFile = CmdInputFactor::getInstance()->getNextFile();
 
   MacroLex lex;
-	JZSetLoggerLevel(JZ_LOG_DEBUG);
   lex.analyzeAFile(toCompileFile);
   LexRecList recList = lex.getRecList();
-  lex.printLexRec();
-	JZSetLoggerLevel(JZ_LOG_TEST);
 
   ASSERT_EQ(17, recList.size());
   ASSERT_STREQ("(", recList[0].word.c_str());
@@ -182,4 +179,78 @@ TEST(MacroLex, macroTest5){
   ASSERT_EQ(2, recList.size());
   ASSERT_STREQ("\"1\"", recList[0].word.c_str());
   ASSERT_STREQ("123", recList[1].word.c_str());
+}
+
+//cover case : ## in var args macro function
+TEST(MacroLex, macroTest6){
+  //this is really a strong test, so don't do it for more than one file every time
+  int argc = 2;
+  char argv0[128] = {0},argv1[128] = {0};
+  strcpy(argv0,"tester");
+  strcpy(argv1,"./test/TestSet/macro_test_6");
+  char* argv[2] = {argv0,argv1};
+
+	JZSetLoggerLevel(JZ_LOG_TEST);
+
+	//analyze command line input
+	CmdInputFactor::getInstance()->analyze(argc, argv);
+
+	//now begin to analyze the files input from command line
+	string toCompileFile = CmdInputFactor::getInstance()->getNextFile();
+
+  MacroLex lex;
+//	JZSetLoggerLevel(JZ_LOG_DEBUG);
+  lex.analyzeAFile(toCompileFile);
+  LexRecList recList = lex.getRecList();
+//	JZSetLoggerLevel(JZ_LOG_TEST);
+
+  ASSERT_EQ(10, recList.size());
+  ASSERT_STREQ("printf", recList[0].word.c_str());
+  ASSERT_STREQ("(", recList[1].word.c_str());
+  ASSERT_STREQ("1", recList[2].word.c_str());
+  ASSERT_STREQ(",", recList[3].word.c_str());
+  ASSERT_STREQ("\"1\"", recList[4].word.c_str());
+  ASSERT_STREQ(")", recList[5].word.c_str());
+
+  ASSERT_STREQ("printf", recList[6].word.c_str());
+  ASSERT_STREQ("(", recList[7].word.c_str());
+  ASSERT_STREQ("1", recList[8].word.c_str());
+  ASSERT_STREQ(")", recList[9].word.c_str());
+}
+
+//cover case : named var args macro function
+TEST(MacroLex, macroTest7){
+  //this is really a strong test, so don't do it for more than one file every time
+  int argc = 2;
+  char argv0[128] = {0},argv1[128] = {0};
+  strcpy(argv0,"tester");
+  strcpy(argv1,"./test/TestSet/macro_test_7");
+  char* argv[2] = {argv0,argv1};
+
+	JZSetLoggerLevel(JZ_LOG_TEST);
+
+	//analyze command line input
+	CmdInputFactor::getInstance()->analyze(argc, argv);
+
+	//now begin to analyze the files input from command line
+	string toCompileFile = CmdInputFactor::getInstance()->getNextFile();
+
+  MacroLex lex;
+//	JZSetLoggerLevel(JZ_LOG_DEBUG);
+  lex.analyzeAFile(toCompileFile);
+  LexRecList recList = lex.getRecList();
+//	JZSetLoggerLevel(JZ_LOG_TEST);
+
+  ASSERT_EQ(10, recList.size());
+  ASSERT_STREQ("printf", recList[0].word.c_str());
+  ASSERT_STREQ("(", recList[1].word.c_str());
+  ASSERT_STREQ("1", recList[2].word.c_str());
+  ASSERT_STREQ(",", recList[3].word.c_str());
+  ASSERT_STREQ("\"1\"", recList[4].word.c_str());
+  ASSERT_STREQ(")", recList[5].word.c_str());
+
+  ASSERT_STREQ("printf", recList[6].word.c_str());
+  ASSERT_STREQ("(", recList[7].word.c_str());
+  ASSERT_STREQ("1", recList[8].word.c_str());
+  ASSERT_STREQ(")", recList[9].word.c_str());
 }
