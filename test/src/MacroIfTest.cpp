d26b3021cef996af389470a0fe35e1eb360f20b2
Johnzeng <Johnz@misfit.com>
2016-04-20 23:34:47 +0800

    add more test case for macro check

--- test/src/MacroIfTest.cpp
+++ test/src/MacroIfTest.cpp
@@ -22,10 +22,7 @@ TEST(MacroLex, macroIfTest1){
 
   MacroLex lex;
 
-	JZSetLoggerLevel(JZ_LOG_DEBUG);
   lex.analyzeAFile(toCompileFile);
-  lex.printLexRec();
-	JZSetLoggerLevel(JZ_LOG_TEST);
 
   LexRecList recList = lex.getRecList();
 
@@ -38,4 +35,37 @@ TEST(MacroLex, macroIfTest1){
   ASSERT_STREQ("(", recList[5].word.c_str());
   ASSERT_STREQ("hello", recList[6].word.c_str());
   ASSERT_STREQ(")", recList[7].word.c_str());
+}
+
+//cover case: simple calculation
+TEST(MacroLex, macroIfTest2){
+  //this is really a strong test, so don't do it for more than one file every time
+  int argc = 2;
+  char argv0[128] = {0},argv1[128] = {0};
+  strcpy(argv0,"tester");
+  strcpy(argv1,"./test/TestSet/macro_if_test_2");
+  char* argv[2] = {argv0,argv1};
+
+
+	//analyze command line input
+	CmdInputFactor::getInstance()->analyze(argc, argv);
+
+	//now begin to analyze the files input from command line
+	string toCompileFile = CmdInputFactor::getInstance()->getNextFile();
+
+  MacroLex lex;
+
+  lex.analyzeAFile(toCompileFile);
+
+  LexRecList recList = lex.getRecList();
+
+  ASSERT_EQ(8, recList.size());
+  ASSERT_STREQ("yes0", recList[0].word.c_str());
+  ASSERT_STREQ("yes1", recList[1].word.c_str());
+  ASSERT_STREQ("yes2", recList[2].word.c_str());
+  ASSERT_STREQ("yes3", recList[3].word.c_str());
+  ASSERT_STREQ("yes4", recList[4].word.c_str());
+  ASSERT_STREQ("yes5", recList[5].word.c_str());
+  ASSERT_STREQ("yes6", recList[6].word.c_str());
+  ASSERT_STREQ("yes7", recList[7].word.c_str());
 }
