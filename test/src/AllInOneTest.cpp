e01a029977a1beb7ffe212f5ddeb5c1aca1ec121
Johnzeng <Johnz@misfit.com>
2016-04-20 23:38:59 +0800

    add all in one test, just make sure we don't make the final case un-runnable

---
+++ test/src/AllInOneTest.cpp
@@ -0,0 +1,27 @@
+#include "Lex.h"
+#include "CmdInputFactor.h"
+#include <gtest/gtest.h>
+#include "JZLogger.h"
+
+//cover case: simple define, basic input
+TEST(Lex, allInOne){
+  //this is really a strong test, so don't do it for more than one file every time
+  int argc = 2;
+  char argv0[128] = {0},argv1[128] = {0};
+  strcpy(argv0,"tester");
+  strcpy(argv1,"./test/TestSet/all_in_one");
+  char* argv[2] = {argv0,argv1};
+
+	JZSetLoggerLevel(JZ_LOG_TEST);
+
+	//analyze command line input
+	CmdInputFactor::getInstance()->analyze(argc, argv);
+
+	//now begin to analyze the files input from command line
+	string toCompileFile = CmdInputFactor::getInstance()->getNextFile();
+
+  Lex lex;
+  lex.analyzeAFile(toCompileFile);
+  LexRecList recList = lex.getRecList();
+
+}
