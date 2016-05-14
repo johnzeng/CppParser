c34fffae7d1ccee49359a7b8758b79abfd10bbb6
Johnzeng <Johnz@misfit.com>
2016-05-14 10:04:34 +0800

    update scripte

--- script/env-search.sh
+++ script/env-search.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 echo '#include <nono.h>' > test.c
-gcc -v test.c 2>&1 | grep '/'|grep -v 'test.c' | grep -v 'InstalledDir' > dirs.txt
+gcc -v test.c 2>&1 | grep '/'| \
+   grep -v 'ignoring nonexistent directory' | grep -v '^GNU C ' | \
+   grep -v "^COLLECT_LTO_WRAPPER" |grep -v '^gcc version' | \
+   grep -v "^Configured with" \
+   grep -v 'test.c' | grep -v 'InstalledDir' > dirs.txt
+
 cat dirs.txt|grep -v '(framework directory)' | sed "s/^ /-I/" >> .factor
 cat dirs.txt|grep '(framework directory)' | sed "s/(framework directory)//"| sed "s/^ /-I/" >> .factor
 gcc -dM -E - < /dev/null | sed "s/#define /-D/"|sed "s/ /=/" >>.factor
