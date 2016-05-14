c3231959a4adeddb4f1dea59404cf70e7ab96601
Johnzeng <Johnz@misfit.com>
2016-05-14 09:46:26 +0800

    add a script to make a full dir list

---
+++ script/header-search.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo '#include <nono.h>' > test.c
+gcc -v test.c 2>&1 | grep '/'|grep -v 'test.c' | grep -v 'InstalledDir' > dirs.txt
+cat dirs.txt|grep -v '(framework directory)' | sed "s/^ /-I/" >> .factor
+cat dirs.txt|grep '(framework directory)' | sed "s/(framework directory)//"| sed "s/^ /-I/" >> .factor
+gcc -dM -E - < /dev/null | sed "s/#define /-D/"|sed "s/ /=/" >>.factor
+
+rm dirs.txt
+rm test.c
