0783d60f3f0766385b8f70829383f3a61e708ad9
Johnzeng <Johnz@misfit.com>
2016-05-16 12:46:13 +0800

    create loop breaker

--- header/LoopBreaker.h
+++ header/LoopBreaker.h
@@ -1,16 +1,20 @@
 #ifndef LOOPBREAKER_H
 #define LOOPBREAKER_H
 
+#include "JZCommonDefine.h"
+using namespace std;
+
 class LoopBreaker {
 public:
   LoopBreaker ();
   virtual ~LoopBreaker ();
 
-  void insert(const string& file, int line, int index);
+  bool insert(const string& file, int line, int index);
   bool check(const string& file, int line, int index);
   void remomve(const string& file, int line, int index);
 
 private:
+  string getKey(const string& file, int line, int index);
   map<string, int> mLoopMark;
 };
 
