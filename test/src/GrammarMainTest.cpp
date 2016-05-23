7f49b855c6d9e6d1fc163a8decbb653913da1555
Johnzeng <Johnz@misfit.com>
2016-05-23 20:38:39 +0800

    add more test case

--- test/src/GrammarMainTest.cpp
+++ test/src/GrammarMainTest.cpp
@@ -34,3 +34,33 @@ TEST(GrammarAnalyzer, Main0)
 
   
 }
+
+TEST(GrammarAnalyzer, Main1)
+{
+  int argc = 2;
+  char argv0[128] = {0},argv1[128] = {0};
+  strcpy(argv0,"tester");
+  strcpy(argv1,"./test/GrammarSample/main_1");
+  char* argv[2] = {argv0,argv1};
+
+	//analyze command line input
+	CmdInputFactor::getInstance()->analyze(argc, argv);
+
+	//now begin to analyze the files input from command line
+	string toCompileFile = CmdInputFactor::getInstance()->getNextFile();
+
+  Lex lex;
+
+  lex.analyzeAFile(toCompileFile);
+
+  LexRecList recList = lex.getRecList();
+
+  GrammarAnalyzer grammar = GrammarAnalyzer(recList);
+
+  uint32 ret = grammar.doAnalyze();
+  JZSetLoggerLevel(JZ_LOG_TEST);
+
+  ASSERT_EQ(eGrmErrNoError, ret);
+
+  
+}
